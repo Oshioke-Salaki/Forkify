d856f14354082a86eb9fc71088cc6e346a9ba797
Oshioke-Salaki <salaki1902@gmail.com>
2022-12-31 18:12:30 -0600

    Implemented search

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,7 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -26,15 +28,25 @@ const controlRecipes = async function() {
 
 const controlSearchResults = async function() {
     try {
-        await model.loadSearchResults('pizza');
-        console.log(model.state.search.results);
+        resultsView.renderSpinner();
+
+        //1. Get search query
+        const query = searchView.getQuery();
+        if (!query) return;
+
+        //2. Load search results
+        await model.loadSearchResults(query);
+
+        //3. Render Results
+        // console.log(model.state.search.results);
+        resultsView.render(model.state.search.results);
     } catch (err) {
         console.log(err);
     }
 };
-controlSearchResults();
 const init = function() {
     recipeView.addHandlerRender(controlRecipes);
+    searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
