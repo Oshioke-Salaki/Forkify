b245ae5555ffef5c3af4037a293b23475446d37b
Oshioke-Salaki <salaki1902@gmail.com>
2023-01-02 09:58:03 -0600

    Updated

--- src/js/controller.js
+++ src/js/controller.js
@@ -61,7 +61,8 @@ const controlServings = function(newServings) {
     //Update reciepe servings(in state)
     model.updateServings(newServings);
     //Update the recipe view
-    recipeView.render(model.state.recipe);
+    // recipeView.render(model.state.recipe);
+    recipeView.update(model.state.recipe);
 };
 const init = function() {
     recipeView.addHandlerRender(controlRecipes);
