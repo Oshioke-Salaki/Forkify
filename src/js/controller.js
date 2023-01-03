48c237baf753daf7f227f786b5c1eb62966e1307
Oshioke-Salaki <salaki1902@gmail.com>
2023-01-02 20:11:50 -0600

    Started implementing bookmarking

--- src/js/controller.js
+++ src/js/controller.js
@@ -68,9 +68,16 @@ const controlServings = function(newServings) {
     // recipeView.render(model.state.recipe);
     recipeView.update(model.state.recipe);
 };
+
+const controlAddBookmark = function() {
+    model.addBookmark(model.state.recipe);
+    console.log(model.state.recipe);
+    recipeView.update(model.state.recipe);
+};
 const init = function() {
     recipeView.addHandlerRender(controlRecipes);
     recipeView.addHandlerUpdateServings(controlServings);
+    recipeView.addHandlerAddBookmark(controlAddBookmark);
     searchView.addHandlerSearch(controlSearchResults);
     paginationView.addHandlerClick(controlPagination);
 };
