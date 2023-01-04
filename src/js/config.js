1cc73dab661c739ab0ab769b9779c37e3d9e98e3
Oshioke-Salaki <salaki1902@gmail.com>
2023-01-03 23:44:23 -0600

    Implemented upload recipe feature

--- src/js/config.js
+++ src/js/config.js
@@ -2,3 +2,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '4ca61f53-3c5c-474a-83da-b4dd7c7be216';
+export const MODAL_CLOSE_SEC = 2.5;
--- src/js/controller.js
+++ src/js/controller.js
@@ -4,9 +4,11 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { MODAL_CLOSE_SEC } from './config';
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -90,6 +92,36 @@ const controlBookamrks = function() {
     bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = async function(newRecipe) {
+    try {
+        //Showing loading spinner
+        addRecipeView.renderSpinner();
+        //Upload the new recipe data
+        await model.uploadRecipe(newRecipe);
+        // console.log(model.state.recipe);
+
+        //render recipe
+        recipeView.render(model.state.recipe);
+
+        //Success message
+        addRecipeView.renderMessage();
+
+        //render bookmark view
+        bookmarksView.render(model.state.bookmarks);
+
+        //change id in url
+        window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+        //close form window
+        setTimeout(function() {
+            addRecipeView.toggleWindow();
+        }, MODAL_CLOSE_SEC * 1000);
+    } catch (err) {
+        console.log(err);
+        addRecipeView.renderError(err.message);
+    }
+};
+
 const init = function() {
     bookmarksView.addHandlerRender(controlBookamrks);
     recipeView.addHandlerRender(controlRecipes);
@@ -97,6 +129,7 @@ const init = function() {
     recipeView.addHandlerAddBookmark(controlAddBookmark);
     searchView.addHandlerSearch(controlSearchResults);
     paginationView.addHandlerClick(controlPagination);
+    addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 
 init();
